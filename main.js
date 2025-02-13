da263d36e633ee1ffba47dc33b2fff4874f77615
ZumeShougo <g0109725rena@gmail.com>
2025-02-13 16:46:14 +0900

    toggle switch

--- main.js
+++ main.js
@@ -21,10 +21,30 @@ function startTimer() {
   }, 10); // 10ミリ秒ごとに更新
 }
 
+function toggleButtons(startDisabled, stopDisabled, resetDisabled) {
+  document.getElementById("start-button").disabled = startDisabled;
+  document.getElementById("stop-button").disabled = stopDisabled;
+  document.getElementById("reset-button").disabled = resetDisabled;
+}
+
 // スタートボタンにクリックイベントを追加
 document.getElementById("start-button").addEventListener("click", startTimer);
+document.getElementById("start-button").addEventListener("click", function () {
+  toggleButtons(true, false, false);
+});
 
 //ストップボタンを押下するとtimerIntervalの繰り返し処理を停止させる
 document.getElementById("stop-button").addEventListener("click", function () {
   clearInterval(timerInterval); // タイマーを停止
+  toggleButtons(false, true, false);
 });
+
+// リセットボタンの処理
+document.getElementById("reset-button").addEventListener("click", function () {
+  clearInterval(timerInterval); // タイマーを停止
+  elapsedTime = 0; // 経過時間をリセット
+  document.querySelector(".stopwatch-result").textContent = "0:0:0:0"; // 表示をリセット
+  toggleButtons(false, true, true);
+});
+
+toggleButtons(false, true, true);
